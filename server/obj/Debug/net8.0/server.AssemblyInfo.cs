0a8b1f6638f63b7246729a6a7f2adb8e12e6096f
NoJi-nx <nejirwanr@gmail.com>
2024-12-18 04:33:41 +0100

    implemented notifiers

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89c0c690400e19b6dec4bbe80dfaf1b82a6b2d5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72eb1f9bb2bceb09adca4c27f61623c21b6259e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
