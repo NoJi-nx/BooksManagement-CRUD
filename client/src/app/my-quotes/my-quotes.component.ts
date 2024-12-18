54ca81baf46758a6e59d41221ce1875c14b96008
NoJi-nx <nejirwanr@gmail.com>
2024-12-18 13:40:40 +0100

    added in displayed quotes

--- client/src/app/my-quotes/my-quotes.component.ts
+++ client/src/app/my-quotes/my-quotes.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-my-quotes',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './my-quotes.component.html',
   styleUrls: []
 })
 export class MyQuotesComponent {
-
+  quotes: string[] = [
+   '“The only limit to our realization of tomorrow is our doubts of today.” – Franklin D. Roosevelt',
+    '“In the middle of every difficulty lies opportunity.” – Albert Einstein',
+    '“Success is not the key to happiness. Happiness is the key to success.” – Albert Schweitzer',
+    '“The future belongs to those who believe in the beauty of their dreams.” – Eleanor Roosevelt',
+    '“Do what you can, with what you have, where you are.” – Theodore Roosevelt'
+  ];
 }
